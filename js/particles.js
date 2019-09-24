9d0c9cffbe89915534b697b332db35da3f7da355
Ronak Doshi <ronakvipul.doshi@iiitb.org>
2019-09-24 21:47:57 +0530

    particle reduction

--- js/particles.js
+++ js/particles.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 150,
+        "value": 70,
         "density": {
           "enable": true,
           "value_area": 800
